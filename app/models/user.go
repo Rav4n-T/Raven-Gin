3242388e0010dfd45f70684b80449fa3551db132
Rav4n-T <leonsgmali@gmail.com>
2024-09-16 12:34:21 +0800

    fix little bug in user model

--- app/models/user.go
+++ app/models/user.go
@@ -7,7 +7,7 @@ type User struct {
 	Username string `json:"username" gorm:"not null;comment:用户名"`
 	Mobile   string `json:"mobile" gorm:"not null;default:18888888888;comment:手机号"`
 	Password string `json:"-" gorm:"not null;comment:密码"`
-	Role     uint   `json:"role" gorm:"not null;default:6;default:customer;comment:角色:1-超管、2-管理员、3-客服、4-客户、5-商家、6-普通用户"`
+	Role     uint   `json:"role" gorm:"not null;default:6;default:6;comment:角色:1-超管、2-管理员、3-客服、4-客户、5-商家、6-普通用户"`
 	Timestamps
 	SoftDelete
 }
--- bootstrap/router.go
+++ bootstrap/router.go
@@ -47,7 +47,7 @@ func RunServer() {
 
 	}()
 
-	quit := make(chan os.Signal)
+	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
 	<-quit
 	log.Println("Shutdown Server...")
